f48387ff55374d3d8a915e575e04a2803ef99b6b
Anup <anupsingh.singh93@gmail.com>
2020-01-06 21:10:23 +0530

    Update text_detection.py

--- text_detection.py
+++ text_detection.py
@@ -6,16 +6,6 @@ import cv2
 # import copy
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str,
-	help="path to input image")
-ap.add_argument("-east", "--east", type=str,
-	help="path to input EAST text detector")
-ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
-	help="minimum probability required to inspect a region")
-ap.add_argument("-w", "--width", type=int, default=320,
-	help="resized image width (should be multiple of 32)")
-ap.add_argument("-e", "--height", type=int, default=320,
-	help="resized image height (should be multiple of 32)")
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
